fd7c6c41f6585b44a01e81c53504fee1473bc875
Naveen Dharmathunga <dnd.pro@outlook.com>
2021-02-14 18:28:49 +0530

    Initial Deploy

--- credentials.py
+++ credentials.py
@@ -3,4 +3,4 @@ bot_token = "1674344480:AAHChseTnqZFNiUhQOWLGXZyQb86r_oNX0w"
 # the username you entered
 bot_user_name = "GrpCareBot"
 # the heroku app link that we will create later
-URL = ""
+URL = "https://grpcarebot.herokuapp.com/"
