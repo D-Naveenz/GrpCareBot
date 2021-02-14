8918bd59928dec9d6b6c7ed06a033b5ec061d151
Naveen Dharmathunga <dnd.pro@outlook.com>
2021-02-14 17:13:20 +0530

    first commit

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# here goes your access token from BotFather
+bot_token = "1674344480:AAHChseTnqZFNiUhQOWLGXZyQb86r_oNX0w"
+# the username you entered
+bot_user_name = "GrpCareBot"
+# the heroku app link that we will create later
+URL = ""
